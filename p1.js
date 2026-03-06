7e5fcf33325bdd542ebc4687680c65d7bd3cac38
badshah317 <shah592jahan@gmail.com>
2026-03-05 21:36:44 -0800

    score commit

---
+++ p1.js
@@ -0,0 +1,12 @@
+// Practice1
+
+// let Mybut1 = document.getElementById("but-error")
+// console.log(Mybut1)
+// function but1(){
+//     console.log("button clicked")
+//     Mybut1.textContent = "Something went wrong!"
+// }
+// but1()
+
+
+// Practice2
